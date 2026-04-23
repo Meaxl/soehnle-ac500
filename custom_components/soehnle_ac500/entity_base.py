679549d65771356d56cd31d54aeaa6bd65197e67
Maximilian <maximilian.brugger@icloud.com>
2026-04-23 10:12:09 +0200

    Update entity_base.py

--- custom_components/soehnle_ac500/entity_base.py
+++ custom_components/soehnle_ac500/entity_base.py
@@ -41,7 +41,7 @@ class AC500EntityBase(Entity):
             name          = device_name,
             manufacturer  = "Soehnle",
             model         = "Airfresh Clean Connect 500",
-            sw_version    = "0.0.2",
+            sw_version    = "0.0.1",
             connections   = {("bluetooth", address)},
         )
 
